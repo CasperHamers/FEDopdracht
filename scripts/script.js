340bcea72b850074d2ee3aca39f27b1eff69c732
CasperHamers <74210218+CasperHamers@users.noreply.github.com>
2024-10-02 20:59:34 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-sdfsfsdfsdf
+var hamburgermenu = document.querySelector("header.button");
+hamburgermenu.onclick = hamburgermenuklik;
+
+function hamburgermenuklik() {
+    if(hamburgermenu.ariaExpanded == "false") {
+        hamburgermenu.ariaExpanded = "true"
+    } else {
+        hamburgermenu.ariaExpanded = "false"
+    }
+}
